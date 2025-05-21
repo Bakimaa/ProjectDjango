c21a6eaecfe7e60e1e636b97838ebafa67eb2218
ethan <ethan.kiene@uha.fr>
2025-05-21 22:09:00 +0200

    changement cool

--- Project_Django/App_Django/views.py
+++ Project_Django/App_Django/views.py
@@ -17,8 +17,8 @@ def ajout(request):
     return render(request, "App_Django/ajout.html", {"form": form_vide})
 
 def affiche(request, id):
-    Jeux = models.Jeux.objects.get(pk=id)
-    return render(request,"App_Django/affiche.html", {"Jeux": Jeux})
+    jeux = models.Jeux.objects.get(pk=id)
+    return render(request,"App_Django/affiche.html", {"Jeux": jeux})
 
 
 def index(request):
@@ -26,22 +26,22 @@ def index(request):
     return render(request,"App_Django/index.html",{"liste" : liste})
 
 def update(request, id):
-    Jeux = models.Jeux.objects.get(pk=id)
-    form = JeuxForm(Jeux.dico())
+    jeux = models.Jeux.objects.get(pk=id)
+    form = JeuxForm(jeux.dico())
     return render(request, "App_Django/ajout.html",{"form" : form, "id" : id})
 
 def updatetraitement(request, id):
     if request.method == "POST":
         form = JeuxForm(request.POST)
         if form.is_valid():
-            Jeux = form.save(commit = False)
-            Jeux.id = id
-            Jeux.save()
+            jeux = form.save(commit = False)
+            jeux.id = id
+            jeux.save()
             return HttpResponseRedirect("/App_Django/")
         else:
             return render(request, "App_Django/ajout.html", {"form": form, "id": id})
 
 def delete(request, id):
-     Jeux = models.Jeux.objects.get(pk=id)
-     Jeux.delete()
+     jeux = models.Jeux.objects.get(pk=id)
+     jeux.delete()
      return HttpResponseRedirect("/App_Django/")
